27e3d5c4d2b0b2ea0421115ef25c262f631c8190
Ruban Prakash <rubanprakash@hotmail.com>
2022-05-05 09:52:39 +0100

    remove web component

--- src/scripts/index.ts
+++
@@ -1,6 +0,0 @@
-import RatingCard from '../components/x-rating-card';
-import ThankYouCard from '../components/x-thank-you-card';
-
-customElements.define('x-rating-card', RatingCard);
-
-customElements.define('x-thank-you-card', ThankYouCard);
